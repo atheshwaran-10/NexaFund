7341ce734605d7d8dc8aa7fb15a545e08275f294
Atheshwaran K <athesh85@gmail.com>
2024-04-02 22:55:27 +0530

    Functionality Completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,9 @@
-"use client";
-import React from "react";
-import { useStateContext } from "@/context";
-
-async function fetchRecentTransactions(address: string) {
-  try {
-   const response = await fetch(
-     `https://cors-anywhere.herokuapp.com/https://api.etherscan.io/api?module=account&action=txlist&address=${address}&sort=desc&apikey=F9XUBTV3KFNC46TTBTCRXX3CT6KMENVJ3Q`,
-   );
-
-    const data = await response.json();
-
-    if (data.status === "1") {
-      const transactions = data.result;
-      console.log(transactions);
-      // Process the transactions as needed
-    } else {
-      console.error("Error retrieving transactions:", data.message);
-    }
-  } catch (error) {
-    console.error("Error retrieving transactions:", error);
-  }
-}
-
-// Call the function with your MetaMask wallet address
+import React from 'react'
 
 const page = () => {
-  const { address } = useStateContext();
-  fetchRecentTransactions(address || "");
-  return <div>page</div>;
-};
+  return (
+    <div>page</div>
+  )
+}
 
-export default page;
+export default page
