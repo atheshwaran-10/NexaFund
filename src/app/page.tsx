4b93fb59439c6b6af09875433aa8224b2bb4ec10
Atheshwaran K <athesh85@gmail.com>
2024-04-06 11:47:39 +0530

    Landing Page Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,15 @@ import Wallets from "./_components/Wallets";
 
 const page = () => {
   return (
-    <div className="relative min-h-screen overflow-hidden bg-[#13131a] text-white">
+    <div className="h-[50rem] w-full  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] ">
+      <div className="relative min-h-screen overflow-hidden  text-white">
+      
+      {/* <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div> */}
       <Navbar />
       <Hero />
       <div className="absolute bottom-0 w-full">
         <Wallets />
+      </div>
       </div>
     </div>
   );
